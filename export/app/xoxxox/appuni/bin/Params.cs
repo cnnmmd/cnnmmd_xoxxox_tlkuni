8bf9c54cfbafe420beccbeff731f75b5dc6e0a88
azsgit <10238943+azsgit@users.noreply.github.com>
2025-07-27 22:26:11 +0900

    wip

---
+++ export/app/xoxxox/appuni/bin/Params.cs
@@ -0,0 +1,8 @@
+namespace Xoxxox {
+  public static class Params {
+    public static string srvadr = "127.0.0.1";
+    public static int srvprt = 10001;
+    public static string pthsnd = "/gpp000";
+    public static string pthrcv = "/sps000";
+  }
+}
